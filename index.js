804c4e0f62268d9555508fbdac2edb8c7dc89ec1
Cherfez <chervasev.leeuwen@gmail.com>
2020-02-20 20:20:59 +0100

    tested server. No joke yet!

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+
+const app = express();
+
+const port = 3000;
+
+const page = `<html>
+  <head>
+    <title>Chervase van Leeuwen</title>
+  </head>
+  <body>
+    <h1>Joke setup</h1>
+    <p>Punchline</p>
+  </body>
+</html>`;
+
+app.get('/', (request, response) => {
+  response.send(page);
+});
+
+function onListen() {
+  console.log(`Listening on: ${port}`);
+}
+
+app.listen(port, onListen);
