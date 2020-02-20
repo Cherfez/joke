26b987974a615787387e9ac4694d7cac7987e97e
Cherfez <chervasev.leeuwen@gmail.com>
2020-02-20 20:53:08 +0100

    added some CSS

--- index.js
+++ index.js
@@ -7,10 +7,31 @@ const port = process.env.PORT || 3000;
 const page = `<html>
   <head>
     <title>Chervase van Leeuwen</title>
+    <style>
+      body {
+        background-color: rgba(167, 251, 146, 0.32);
+        text-align: center;
+      }
+
+      h1 {
+        color: #155704;
+        font-family: Arial;
+        padding: 1rem;
+        font-size: 3rem;
+        margin-top: 7rem;
+      }
+
+      p {
+        font-family: Arial;
+        font-size: 2.5rem;
+        color: orange;
+        padding-top: 3rem;
+      }
+    </style>
   </head>
   <body>
-    <h1>Joke setup</h1>
-    <p>Punchline</p>
+    <h1>The only web developers happy to find a bug</h1>
+    <p>Spiders</p>
   </body>
 </html>`;
 
